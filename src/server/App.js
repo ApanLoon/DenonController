484f1713c5d614928b64051613b7f304bf4a044b
ApanLoon <apanloon@live.co.uk>
2023-04-09 13:20:21 +0200

    Created properties for Denon input names.

--- src/server/App.js
+++ src/server/App.js
@@ -1,46 +1,52 @@
 
-import { DenonOptions, DenonDevice, DenonEvent } from "./Denon.js";
+import { DenonOptions, DenonDevice, DenonEvent, DenonInput } from "./Denon.js";
 import { MiniacApi, MiniacEvent } from "./MiniacApi.js";
 
 console.log ("App: Starting up...");
 
-const amp_InputNames =
-[
-    {denon: "TUNER"    , miniac: "TUNER"},
-    {denon: "DVD"      , miniac: "DVD"},
-    {denon: "BD"       , miniac: "BD"},
-    {denon: "TV"       , miniac: "TV"},
-    {denon: "SAT/CBL"  , miniac: "SAT/CBL"},
-    {denon: "MPLAY"    , miniac: "SHIELD"},
-    {denon: "GAME"     , miniac: "MINIAC"},
-    {denon: "AUX1"     , miniac: "AUX1"},
-    {denon: "NET"      , miniac: "NET"},
-    {denon: "PANDORA"  , miniac: "PANDORA"},
-    {denon: "SIRIUSXM" , miniac: "SIRIUSXM"},
-    {denon: "LASTFM"   , miniac: "LASTFM"},
-    {denon: "FLICKR"   , miniac: "FLICKR"},
-    {denon: "FAVORITES", miniac: "FAVORITES"},
-    {denon: "IRADIO"   , miniac: "IRADIO"},
-    {denon: "SERVER"   , miniac: "SERVER"},
-    {denon: "USB/IPOD" , miniac: "USB/IPOD"},
-    {denon: "USB"      , miniac: "USB"},
-    {denon: "IPD"      , miniac: "IPD"},
-    {denon: "IRP"      , miniac: "IRP"},
-    {denon: "FVP"      , miniac: "FVP"}
-];
+const config =
+{
+    amp_Host: "192.168.0.52",
+    amp_Port: 23,
+    amp_InputNames:
+    [
+        {denon: DenonInput.Phono      , miniac: "PHONO"     },
+        {denon: DenonInput.Cd         , miniac: "CD"        },
+        {denon: DenonInput.Dvd        , miniac: "DVD"       },
+        {denon: DenonInput.Bluray     , miniac: "BLURAY"    },
+        {denon: DenonInput.Tv         , miniac: "TV"        },
+        {denon: DenonInput.SatCbl     , miniac: "SAT/CBL"   },
+        {denon: DenonInput.MediaPlayer, miniac: "SHIELD"    },
+        {denon: DenonInput.Game       , miniac: "MINIAC"    },
+        {denon: DenonInput.Tuner      , miniac: "TUNER"     },
+        {denon: DenonInput.HdRadio    , miniac: "HDRADIO"   },
+        {denon: DenonInput.IRadio     , miniac: "IRADIO"    },
+        {denon: DenonInput.Server     , miniac: "SERVER"    },
+        {denon: DenonInput.Favorites  , miniac: "FAVORITES" },
+        {denon: DenonInput.Aux1       , miniac: "AUX1"      },
+        {denon: DenonInput.Aux2       , miniac: "AUX2"      },
+        {denon: DenonInput.Aux3       , miniac: "AUX3"      },
+        {denon: DenonInput.Aux4       , miniac: "AUX4"      },
+        {denon: DenonInput.Aux5       , miniac: "AUX5"      },
+        {denon: DenonInput.Aux6       , miniac: "AUX6"      },
+        {denon: DenonInput.Aux7       , miniac: "AUX7"      },
+        {denon: DenonInput.Net        , miniac: "NET"       },
+        {denon: DenonInput.Bluetooth  , miniac: "BLUETOOTH" }
+    ]
+}
 
 function amp_InputNameFromDenon(input)
 {
-    return amp_InputNames.find(x=>x.denon === input)?.miniac;
+    return config.amp_InputNames.find(x=>x.denon === input)?.miniac;
 }
 
 function amp_InputNameFromMiniac(input)
 {
-    return amp_InputNames.find(x=>x.miniac === input)?.denon;
+    return config.amp_InputNames.find(x=>x.miniac === input)?.denon;
 }
 
 const miniacApi = new MiniacApi();
-const denon = new DenonDevice({ host: "192.168.0.52", port: 23 });
+const denon = new DenonDevice({ host: config.amp_Host, port: config.amp_Port });
 
 denon.on(DenonEvent.PowerState,    isOn  => miniacApi.amp_SendPowerState(isOn));
 denon.on(DenonEvent.SelectedInput, input => miniacApi.amp_SendSelectedInput(amp_InputNameFromDenon (input)));
--- src/server/Denon.js
+++ src/server/Denon.js
@@ -13,6 +13,33 @@ export class DenonEvent
     static get SelectedInput() { return "selectedinput"; }
 }
 
+/// AVR X3500H, X2500H, X1500H, S940H, S740H, S640H
+export class DenonInput
+{
+    static get Phono()       { return "PHONO";     }
+    static get Cd()          { return "CD";        }
+    static get Dvd()         { return "DVD";       }
+    static get Bluray()      { return "BD";        }
+    static get Tv()          { return "TV";        }
+    static get SatCbl()      { return "SAT/CBL";   }
+    static get MediaPlayer() { return "MPLAY";     }
+    static get Game()        { return "GAME";      }
+    static get Tuner()       { return "TUNER";     }
+    static get HdRadio()     { return "HDRADIO";   } // North America model only
+    static get IRadio()      { return "IRADIO";    }
+    static get Server()      { return "SERVER";    }
+    static get Favorites()   { return "FAVORITES"; }
+    static get Aux1()        { return "AUX1";      }
+    static get Aux2()        { return "AUX2";      }
+    static get Aux3()        { return "AUX3";      }
+    static get Aux4()        { return "AUX4";      }
+    static get Aux5()        { return "AUX5";      }
+    static get Aux6()        { return "AUX6";      }
+    static get Aux7()        { return "AUX7";      }
+    static get Net()         { return "NET";       }
+    static get Bluetooth()   { return "BT";        }
+}
+
 export class DenonDevice extends EventEmitter
 {
 
@@ -48,28 +75,29 @@ export class DenonDevice extends EventEmitter
                     case "PWON\r":      this.emit(DenonEvent.PowerState, true ); break;
                     case "PWSTANDBY\r": this.emit(DenonEvent.PowerState, false); break;
 
-                    case "SITUNER\r":     this.emit(DenonEvent.SelectedInput, "TUNER"     ); break;
-                    case "SIDVD\r":       this.emit(DenonEvent.SelectedInput, "DVD"       ); break;
-                    case "SIBD\r":        this.emit(DenonEvent.SelectedInput, "BD"        ); break;
-                    case "SITV\r":        this.emit(DenonEvent.SelectedInput, "TV"        ); break;
-                    case "SISAT/CBL\r":   this.emit(DenonEvent.SelectedInput, "SAT/CBL"   ); break;
-                    case "SIMPLAY\r":     this.emit(DenonEvent.SelectedInput, "MPLAY"     ); break;
-                    case "SIGAME\r":      this.emit(DenonEvent.SelectedInput, "GAME"      ); break;
-                    case "SIAUX1\r":      this.emit(DenonEvent.SelectedInput, "AUX1"      ); break;
-                    case "SINET\r":       this.emit(DenonEvent.SelectedInput, "NET"       ); break;
-                    case "SIPANDORA\r":   this.emit(DenonEvent.SelectedInput, "PANDORA"   ); break;
-                    case "SISIRIUSXM\r":  this.emit(DenonEvent.SelectedInput, "SIRIUSXM"  ); break;
-                    case "SILASTFM\r":    this.emit(DenonEvent.SelectedInput, "LASTFM"    ); break;
-                    case "SIFLICKR\r":    this.emit(DenonEvent.SelectedInput, "FLICKR"    ); break;
-                    case "SIFAVORITES\r": this.emit(DenonEvent.SelectedInput, "FAVORITES" ); break;
-                    case "SIIRADIO\r":    this.emit(DenonEvent.SelectedInput, "IRADIO"    ); break;
-                    case "SISERVER\r":    this.emit(DenonEvent.SelectedInput, "SERVER"    ); break;
-                    case "SIUSB/IPOD\r":  this.emit(DenonEvent.SelectedInput, "USB/IPOD"  ); break;
-                    case "SIUSB\r":       this.emit(DenonEvent.SelectedInput, "USB"       ); break;
-                    case "SIIPD\r":       this.emit(DenonEvent.SelectedInput, "IPD"       ); break;
-                    case "SIIRP\r":       this.emit(DenonEvent.SelectedInput, "IRP"       ); break;
-                    case "SIFVP\r":       this.emit(DenonEvent.SelectedInput, "FVP"       ); break;
-
+                    case "SIPHONO\r":     this.emit(DenonEvent.SelectedInput, DenonInput.Phono       ); break;
+                    case "SICD\r":        this.emit(DenonEvent.SelectedInput, DenonInput.Cd          ); break;
+                    case "SIDVD\r":       this.emit(DenonEvent.SelectedInput, DenonInput.Dvd         ); break;
+                    case "SIBD\r":        this.emit(DenonEvent.SelectedInput, DenonInput.Bluray      ); break;
+                    case "SITV\r":        this.emit(DenonEvent.SelectedInput, DenonInput.Tv          ); break;
+                    case "SISAT/CBL\r":   this.emit(DenonEvent.SelectedInput, DenonInput.SatCbl      ); break;
+                    case "SIMPLAY\r":     this.emit(DenonEvent.SelectedInput, DenonInput.MediaPlayer ); break;
+                    case "SIGAME\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Game        ); break;
+                    case "SITUNER\r":     this.emit(DenonEvent.SelectedInput, DenonInput.Tuner       ); break;
+                    case "SIHDRADIO\r":   this.emit(DenonEvent.SelectedInput, DenonInput.HdRadio     ); break;
+                    case "SIIRADIO\r":    this.emit(DenonEvent.SelectedInput, DenonInput.IRadio      ); break;
+                    case "SISERVER\r":    this.emit(DenonEvent.SelectedInput, DenonInput.Server      ); break;
+                    case "SIFAVORITES\r": this.emit(DenonEvent.SelectedInput, DenonInput.Favorites   ); break;
+                    case "SIAUX1\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux1        ); break;
+                    case "SIAUX2\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux2        ); break;
+                    case "SIAUX3\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux3        ); break;
+                    case "SIAUX4\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux4        ); break;
+                    case "SIAUX5\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux5        ); break;
+                    case "SIAUX6\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux6        ); break;
+                    case "SIAUX7\r":      this.emit(DenonEvent.SelectedInput, DenonInput.Aux7        ); break;
+                    case "SINET\r":       this.emit(DenonEvent.SelectedInput, DenonInput.Net         ); break;
+                    case "SIBT\r":        this.emit(DenonEvent.SelectedInput, DenonInput.Bluetooth   ); break;
+                
                     default: console.log ("DenonDevice: Received: ", msg); break;
                 }
             });
