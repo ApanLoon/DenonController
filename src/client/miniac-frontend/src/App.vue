5267d919b48b53481f59579fc6fa48bce7c628f3
ApanLoon <apanloon@live.co.uk>
2023-04-16 09:42:02 +0200

    Indicate connection status

--- src/client/miniac-frontend/src/App.vue
+++ src/client/miniac-frontend/src/App.vue
@@ -38,5 +38,44 @@ function closePage() : void
     <PlaylistPage v-if="currentPage === 'playlist'" @close="closePage()"></PlaylistPage>
     <LibraryPage  v-if="currentPage === 'library'"  @close="closePage()"></LibraryPage>
   </main>
+  <Transition>
+    <div class="noconnection" v-if="miniac.isConnected.value === false">Not connected</div>
+  </Transition>
 </template>
 
+<style scoped>
+
+.noconnection
+{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: var(--color-background-mute);
+  opacity: 0.9;
+
+  font-size: 7rem;
+}
+
+.v-enter-active,
+.v-leave-active
+{
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to
+{
+  opacity: 0;
+}
+</style>
